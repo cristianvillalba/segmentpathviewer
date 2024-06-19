773cca9b6d633cba80eb386282eb4c9d433d8ca8
Cristian Villalba <cristian.villalba@gmail.com>
2024-06-19 17:51:51 -0300

    change rendering

--- src/segment/PivotNoLimit.java
+++ src/segment/PivotNoLimit.java
@@ -7,7 +7,9 @@ package segment;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
@@ -19,13 +21,16 @@ import com.jme3.scene.shape.Box;
 public class PivotNoLimit {
     private Node pivot;
     private Node pivotEnd;
+    private boolean hassemiplane = false;
     
-    public PivotNoLimit(Node father, AssetManager assetManager, int i, float beta)
+    public PivotNoLimit(Node father, AssetManager assetManager, int i, float beta, boolean plane)
     {
         //Box mesh = new Box(1.0f, 0.1f, 0.1f); //a segment with an extension of 2.0
         Box mesh = new Box(0.5f, 0.1f, 0.1f); //a segment with an extension of 1.0
         Geometry geo = new Geometry("line", mesh);
-       
+        
+        hassemiplane = plane;
+        
         Material mat3 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat3.setColor("Color", ColorRGBA.Green);
         geo.setMaterial(mat3);
@@ -39,6 +44,24 @@ public class PivotNoLimit {
         
         pivot.attachChild(geo);
         pivot.attachChild(pivotEnd);
+        
+        if (hassemiplane)
+        {
+            Box meshplane = new Box(5.0f, 5.0f, 0.1f);
+            Geometry geoplane = new Geometry("plane", meshplane);
+            
+            Material mat4 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            mat4.setColor("Color", new ColorRGBA(1.0f, 1.0f, 1.0f, 0.01f));
+            mat4.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+            mat4.getAdditionalRenderState().setDepthTest(false);
+           
+            geoplane.setMaterial(mat4);
+            geoplane.setQueueBucket(RenderQueue.Bucket.Transparent);
+            
+            geoplane.setLocalTranslation(5.0f, 0.0f, 0.0f);
+        
+            pivot.attachChild(geoplane);
+        }
 
         //use segment proportion equal to ETA function
         float base = 1.0f/(i + 1.0f);
